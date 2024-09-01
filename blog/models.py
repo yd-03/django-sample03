676311a8f3e367489c6fde0c69f65765ecb4d86b
yd-03 <3nu4ydye57@gmail.com>
2024-09-01 22:05:00 +0900

    feat: ブログ投稿モデルのフィールド名を修正

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,12 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    body = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    published_at = models.DateTimeField(blank=True, null=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_at = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
