b7c1563e3fec93f9ed4bb07690101531e4f59af4
yd-03 <3nu4ydye57@gmail.com>
2024-09-02 22:15:27 +0900

    feat: ブログ投稿フォームと詳細ページを追加

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
+from .forms import PostForm
 from .models import Post
 
 
@@ -9,3 +10,22 @@ def post_list(request):
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, "blog/post_edit.html", {"form": form})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
